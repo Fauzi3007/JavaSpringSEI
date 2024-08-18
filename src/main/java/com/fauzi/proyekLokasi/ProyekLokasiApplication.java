35c9e9fd86b5a4707b8915a49d6e670b93f53068
USER <USER@LENOVO>
2024-08-18 10:54:54 +0700

    fix

---
+++ src/main/java/com/fauzi/proyekLokasi/ProyekLokasiApplication.java
@@ -0,0 +1,13 @@
+package com.fauzi.proyekLokasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyekLokasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyekLokasiApplication.class, args);
+	}
+
+}
